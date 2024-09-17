f809cf4cfa7c38b0f4293ba956c5f59e16dcc0da
Joshua Daniel Pratt Nielsen <jdpnielsen@gmail.com>
2024-09-17 11:43:55 +0200

    docs: update examples to match new api

--- examples/cerror.js
+++ examples/cerror.js
@@ -3,7 +3,7 @@ const { CError } = require('../');
 
 const filename = '/nonexistent';
 fs.stat(filename, function(err1) {
-	const err2 = new CError(`stat "${filename}" failed`, err1);
+	const err2 = new CError(`stat "${filename}" failed`, { cause: err1 });
 	console.error(err2.message);
 	console.error(CError.cause(err2).message);
 });
--- examples/fullStack.mjs
+++ examples/fullStack.mjs
@@ -2,6 +2,6 @@ const { CError } = require('../');
 
 const err1 = new CError('something bad happened');
 /* ... */
-const err2 = new CError('something really bad happened here', err1);
+const err2 = new CError('something really bad happened here', { cause: err1 });
 
 console.log(CError.fullStack(err2));
